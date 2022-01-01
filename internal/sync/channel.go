396084482e13010dbd0354957b092adee0530356
Christopher Schleiden <cschleiden@live.de>
2022-01-01 09:10:10 +0100

    Rename channel methods

--- internal/sync/channel.go
+++ internal/sync/channel.go
@@ -56,7 +56,7 @@ func (c *channel) Send(ctx context.Context, v interface{}) {
 	cr := getCoState(ctx)
 
 	for {
-		if c.send(v) {
+		if c.trySend(v) {
 			return
 		}
 
@@ -65,7 +65,7 @@ func (c *channel) Send(ctx context.Context, v interface{}) {
 }
 
 func (c *channel) SendNonblocking(ctx context.Context, v interface{}) bool {
-	return c.send(v)
+	return c.trySend(v)
 }
 
 func (c *channel) Receive(ctx context.Context, vptr interface{}) (more bool) {
@@ -75,8 +75,8 @@ func (c *channel) Receive(ctx context.Context, vptr interface{}) (more bool) {
 	receivedValue := false
 
 	for {
-		// Try to receive from buffered channel
-		if c.receive(vptr) {
+		// Try to receive from buffered channel or blocked sender
+		if c.tryReceive(vptr) {
 			return !c.closed
 		}
 
@@ -84,8 +84,12 @@ func (c *channel) Receive(ctx context.Context, vptr interface{}) (more bool) {
 		if !addedListener {
 			cb := func(v interface{}) {
 				receivedValue = true
+
+				// TODO: Assert pointer
+				// TODO: Extract assignment logic
 				reflect.ValueOf(vptr).Elem().Set(reflect.ValueOf(v))
 			}
+
 			c.receivers = append(c.receivers, cb)
 			addedListener = true
 		}
@@ -100,18 +104,21 @@ func (c *channel) Receive(ctx context.Context, vptr interface{}) (more bool) {
 }
 
 func (c *channel) ReceiveNonblocking(ctx context.Context, vptr interface{}) (ok bool) {
-	return c.receive(vptr)
+	return c.tryReceive(vptr)
 }
 
 func (c *channel) hasValue() bool {
 	return len(c.c) > 0
 }
 
-func (c *channel) send(v interface{}) bool {
+func (c *channel) trySend(v interface{}) bool {
+	// If closed, we can't send, exit.
 	if c.closed {
 		panic("channel closed")
 	}
 
+	// Are there any existing blocked receivers? If so, unblock the first one with
+	// the value.
 	if len(c.receivers) > 0 {
 		r := c.receivers[0]
 		c.receivers[0] = nil
@@ -120,16 +127,17 @@ func (c *channel) send(v interface{}) bool {
 		return true
 	}
 
-	// No waiting receiver, if we have capacity try to add
+	// No waiting receiver, if we have capacity try to add the value to the buffer
 	if c.hasCapacity() {
 		c.c = append(c.c, v)
 		return true
 	}
 
+	// No receiver waiting and no capacity, we can't send.
 	return false
 }
 
-func (c *channel) receive(vptr interface{}) bool {
+func (c *channel) tryReceive(vptr interface{}) bool {
 	if c.closed {
 		panic("channel closed")
 	}
@@ -139,6 +147,7 @@ func (c *channel) receive(vptr interface{}) bool {
 		c.c = c.c[1:]
 
 		// TODO: Assert pointer
+		// TODO: Extract assignment logic
 		reflect.ValueOf(vptr).Elem().Set(reflect.ValueOf(v))
 		return true
 	}
