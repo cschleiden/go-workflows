08010e470c2f1e70c0011aa7676b6c6ce8f7f15b
Christopher Schleiden <cschleiden@live.de>
2022-02-28 21:18:52 -0800

    Comment interface

--- pkg/core/task/workflow.go
+++ pkg/core/task/workflow.go
@@ -13,10 +13,16 @@ const (
 )
 
 type Workflow struct {
+	// WorkflowInstance is the workflow instance that this task is for
 	WorkflowInstance core.WorkflowInstance
 
+	// Kind defines what kind of task this is. A Continuation task only contains
+	// new events and not the full history. By default the history is included.
 	Kind Kind
 
-	History   []history.Event
+	// History are the events that have been executed so far
+	History []history.Event
+
+	// NewEvents are new events since the last task execution
 	NewEvents []history.Event
 }
