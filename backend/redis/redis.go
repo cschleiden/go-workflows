46bb4e35c9226786ed3dfc7b420cf766eefadae1
Christopher Schleiden <cschleiden@live.de>
2022-04-03 19:25:16 -0700

    Empty backend implementation

---
+++ backend/redis/redis.go
@@ -0,0 +1,61 @@
+package redis
+
+import (
+	"context"
+
+	"github.com/cschleiden/go-workflows/backend"
+	"github.com/cschleiden/go-workflows/internal/core"
+	"github.com/cschleiden/go-workflows/internal/history"
+	"github.com/cschleiden/go-workflows/internal/task"
+)
+
+func NewRedisBackend() backend.Backend {
+	return &redisBackend{}
+}
+
+type redisBackend struct {
+}
+
+func (*redisBackend) CancelWorkflowInstance(ctx context.Context, instance core.WorkflowInstance) error {
+	panic("unimplemented")
+}
+
+func (*redisBackend) CompleteActivityTask(ctx context.Context, instance core.WorkflowInstance, activityID string, event history.Event) error {
+	panic("unimplemented")
+}
+
+func (*redisBackend) CompleteWorkflowTask(ctx context.Context, instance core.WorkflowInstance, state backend.WorkflowState, executedEvents []history.Event, activityEvents []history.Event, workflowEvents []history.WorkflowEvent) error {
+	panic("unimplemented")
+}
+
+func (*redisBackend) CreateWorkflowInstance(ctx context.Context, event history.WorkflowEvent) error {
+	panic("unimplemented")
+}
+
+func (*redisBackend) ExtendActivityTask(ctx context.Context, activityID string) error {
+	panic("unimplemented")
+}
+
+func (*redisBackend) ExtendWorkflowTask(ctx context.Context, instance core.WorkflowInstance) error {
+	panic("unimplemented")
+}
+
+func (*redisBackend) GetActivityTask(ctx context.Context) (*task.Activity, error) {
+	panic("unimplemented")
+}
+
+func (*redisBackend) GetWorkflowInstanceHistory(ctx context.Context, instance core.WorkflowInstance) ([]history.Event, error) {
+	panic("unimplemented")
+}
+
+func (*redisBackend) GetWorkflowInstanceState(ctx context.Context, instance core.WorkflowInstance) (backend.WorkflowState, error) {
+	panic("unimplemented")
+}
+
+func (*redisBackend) GetWorkflowTask(ctx context.Context) (*task.Workflow, error) {
+	panic("unimplemented")
+}
+
+func (*redisBackend) SignalWorkflow(ctx context.Context, instanceID string, event history.Event) error {
+	panic("unimplemented")
+}
