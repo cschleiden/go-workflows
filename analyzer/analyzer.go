3b578ee473bf292f2f712ae4df30186352c50d54
Christopher Schleiden <cschleiden@live.de>
2022-05-06 21:06:42 +0000

    Add remaining checks to analyzer

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -1,6 +1,7 @@
 package analyzer
 
 import (
+	"fmt"
 	"go/ast"
 	"go/types"
 
@@ -19,56 +20,138 @@ var Analyzer = &analysis.Analyzer{
 func run(pass *analysis.Pass) (interface{}, error) {
 	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
-	nodeFilter := []ast.Node{(*ast.FuncDecl)(nil)}
-
-	inspector.Preorder(nodeFilter, func(node ast.Node) {
-		funcDecl := node.(*ast.FuncDecl)
-
-		if !isWorkflow(funcDecl) {
-			return
+	inspector.Nodes(nil, func(node ast.Node, push bool) bool {
+		if !push {
+			return false
 		}
 
-		// Check return types
-		if funcDecl.Type.Results == nil || len(funcDecl.Type.Results.List) == 0 {
-			pass.Reportf(funcDecl.Pos(), "workflow %q doesn't return anything. needs to return at least `error`", funcDecl.Name.Name)
-		} else {
-			if len(funcDecl.Type.Results.List) > 2 {
-				pass.Reportf(funcDecl.Pos(), "workflow %q returns more than two values", funcDecl.Name.Name)
-				return
+		switch n := node.(type) {
+		case *ast.FuncDecl:
+			// Only check functions that look like workflows
+			if !isWorkflow(n) {
+				return false
 			}
 
-			lastResult := funcDecl.Type.Results.List[len(funcDecl.Type.Results.List)-1]
-			if types.ExprString(lastResult.Type) != "error" {
-				pass.Reportf(funcDecl.Pos(), "workflow %q doesn't return `error` as last return value", funcDecl.Name.Name)
-			}
-		}
-
-		// Check for various errors in the workflow body
-		for _, stmt := range funcDecl.Body.List {
-			switch stmt := stmt.(type) {
-			// Check for map iterations
-			case *ast.RangeStmt:
-				{
-					t := pass.TypesInfo.TypeOf(stmt.X)
-					if t == nil {
-						continue
-					}
-
-					if _, ok := t.(*types.Map); !ok {
-						continue
-					}
-
-					pass.Reportf(stmt.Pos(), "iterating over a map is not deterministic and not allowed in workflows")
+			// Check return types
+			if n.Type.Results == nil || len(n.Type.Results.List) == 0 {
+				pass.Reportf(n.Pos(), "workflow `%v` doesn't return anything. needs to return at least `error`", n.Name.Name)
+			} else {
+				if len(n.Type.Results.List) > 2 {
+					pass.Reportf(n.Pos(), "workflow `%v` returns more than two values", n.Name.Name)
+					return true
 				}
 
-			// Check for `go` statements
-			case *ast.GoStmt:
-				pass.Reportf(stmt.Pos(), "use workflow.Go instead of `go` in workflows")
+				lastResult := n.Type.Results.List[len(n.Type.Results.List)-1]
+				if types.ExprString(lastResult.Type) != "error" {
+					pass.Reportf(n.Pos(), "workflow `%v` doesn't return `error` as last return value", n.Name.Name)
+				}
+			}
+
+			funcScope := pass.TypesInfo.Scopes[n.Type]
+			if funcScope != nil {
+				checkVarsInScope(pass, funcScope)
+			}
+
+			// Continue with the function's children
+			return true
+
+		case *ast.RangeStmt:
+			{
+				t := pass.TypesInfo.TypeOf(n.X)
+				if t == nil {
+					break
+				}
+
+				switch t.(type) {
+				case *types.Map:
+					pass.Reportf(n.Pos(), "iterating over a `map` is not deterministic and not allowed in workflows")
+
+				case *types.Chan:
+					pass.Reportf(n.Pos(), "using native channels is not allowed in workflows, use `workflow.Channel` instead")
+				}
+
+				// checkStatements(pass, n.Body.List)
+			}
+
+		case *ast.SelectStmt:
+			pass.Reportf(n.Pos(), "`select` statements are not allowed in workflows, use `workflow.Select` instead")
+
+		case *ast.GoStmt:
+			pass.Reportf(n.Pos(), "use `workflow.Go` instead of `go` in workflows")
+
+		case *ast.CallExpr:
+			var pkg *ast.Ident
+			var id *ast.Ident
+			switch fun := n.Fun.(type) {
+			case *ast.SelectorExpr:
+				pkg, _ = fun.X.(*ast.Ident)
+				id = fun.Sel
+			}
+
+			if pkg == nil || id == nil {
+				break
+			}
+
+			pkgInfo := pass.TypesInfo.Uses[pkg]
+			pkgName, _ := pkgInfo.(*types.PkgName)
+			if pkgName == nil {
+				break
+			}
+
+			switch pkgName.Imported().Path() {
+			case "time":
+				switch id.Name {
+				case "Now":
+					pass.Reportf(n.Pos(), "`time.Now` is not allowed in workflows, use `workflow.Now` instead")
+				case "Sleep":
+					pass.Reportf(n.Pos(), "`time.Sleep` is not allowed in workflows, use `workflow.Sleep` instead")
+				}
 			}
 		}
+
+		// Continue with the children
+		return true
 	})
 
 	return nil, nil
+}
+
+func checkVarsInScope(pass *analysis.Pass, scope *types.Scope) {
+	for _, name := range scope.Names() {
+		obj := scope.Lookup(name)
+		switch t := obj.Type().(type) {
+		case *types.Chan:
+			pass.Reportf(obj.Pos(), "using native channels is not allowed in workflows, use `workflow.Channel` instead")
+
+		case *types.Named:
+			checkNamed(pass, obj, t)
+
+		case *types.Pointer:
+			if named, ok := t.Elem().(*types.Named); ok {
+				checkNamed(pass, obj, named)
+			}
+		}
+	}
+
+	for i := 0; i < scope.NumChildren(); i++ {
+		checkVarsInScope(pass, scope.Child(i))
+	}
+}
+
+func checkNamed(pass *analysis.Pass, ref types.Object, named *types.Named) {
+	if obj := named.Obj(); obj != nil {
+		if pkg := obj.Pkg(); pkg != nil {
+			fmt.Println(pkg.Path(), obj.Name(), obj.Id())
+
+			switch pkg.Path() {
+			case "sync":
+				if obj.Name() == "WaitGroup" {
+					pass.Reportf(ref.Pos(), "using `sync.WaitGroup` is not allowed in workflows, use `workflow.WaitGroup` instead")
+				}
+			}
+		}
+	}
+
 }
 
 func isWorkflow(funcDecl *ast.FuncDecl) bool {
--- analyzer/testdata/src/p/p.go
+++ analyzer/testdata/src/p/p.go
@@ -1,12 +1,21 @@
+//nolint
 package p
 
 // Work around module issues. The analyzer just looks for `workflow.Context` currently
 import (
+	"context"
 	workflow "context"
 	"fmt"
+	"time"
+
+	"sync"
 )
 
+var foo int = 42
+
 func wf(ctx workflow.Context) error {
+	fmt.Println(foo)
+
 	return nil
 }
 
@@ -14,15 +23,15 @@ func wfWithResult(ctx workflow.Context) (string, error) {
 	return "", nil
 }
 
-func wfWithTooManyResults(ctx workflow.Context) (int, string, error) { // want "workflow \"wfWithTooManyResults\" returns more than two values"
+func wfWithTooManyResults(ctx workflow.Context) (int, string, error) { // want "workflow `wfWithTooManyResults` returns more than two values"
 	return 42, "", nil
 }
 
-func wfWrongOrder(ctx workflow.Context) (error, string) { // want "workflow \"wfWrongOrder\" doesn't return `error` as last return value"
+func wfWrongOrder(ctx workflow.Context) (error, string) { // want "workflow `wfWrongOrder` doesn't return `error` as last return value"
 	return nil, ""
 }
 
-func wfWithoutReturn(ctx workflow.Context) { // want "workflow \"wfWithoutReturn\" doesn't return anything. needs to return at least `error`"
+func wfWithoutReturn(ctx workflow.Context) { // want "workflow `wfWithoutReturn` doesn't return anything. needs to return at least `error`"
 }
 
 func wfIteratingOverMap(ctx workflow.Context) error {
@@ -30,7 +39,7 @@ func wfIteratingOverMap(ctx workflow.Context) error {
 
 	fmt.Println("log")
 
-	for _, v := range x { // want "iterating over a map is not deterministic and not allowed in workflows"
+	for _, v := range x { // want "iterating over a `map` is not deterministic and not allowed in workflows"
 		if v == "a" {
 			return nil
 		}
@@ -40,9 +49,65 @@ func wfIteratingOverMap(ctx workflow.Context) error {
 }
 
 func wfUsingGoRoutine(ctx workflow.Context) error {
-	go func() { // want "use workflow.Go instead of `go` in workflows"
+	go func() { // want "use `workflow.Go` instead of `go` in workflows"
 		fmt.Println("hello")
 	}()
+
+	return nil
+}
+
+func wfUsingSelect(ctx workflow.Context) error {
+	select { // want "`select` statements are not allowed in workflows, use `workflow.Select` instead"
+	}
+
+	return nil
+}
+
+func wfChanRange(ctx workflow.Context) error {
+	for range make(chan int, 0) { // want "using native channels is not allowed in workflows, use `workflow.Channel` instead"
+		if ctx == nil {
+			v := make(chan int, 0) // want "using native channels is not allowed in workflows, use `workflow.Channel` instead"
+			<-v
+
+			for range make(chan int, 0) { // want "using native channels is not allowed in workflows, use `workflow.Channel` instead"
+			}
+		}
+	}
+
+	return nil
+}
+
+func wfVarUsage(ctx workflow.Context) error {
+	var wg sync.WaitGroup // want "`sync.WaitGroup` is not allowed in workflows"
+	wg.Wait()
+
+	wg2 := &sync.WaitGroup{} // want "`sync.WaitGroup` is not allowed in workflows"
+	wg2.Wait()
+
+	return nil
+}
+
+func wfChanFuncDecl(ctx workflow.Context) error {
+	x := func() {
+		select {} // want "`select` statements are not allowed in workflows, use `workflow.Select` instead"
+	}
+
+	x()
+
+	return nil
+}
+
+func wfFunctionUsage(ctx workflow.Context) error {
+	time.Sleep(10 * time.Second) // want "`time.Sleep` is not allowed in workflows, use `workflow.Sleep` instead"
+	fmt.Println(time.Now())      // want "`time.Now` is not allowed in workflows, use `workflow.Now` instead"
+
+	return nil
+}
+
+func activity(ctx context.Context) error {
+	go fmt.Println("test")
+
+	select {}
 
 	return nil
 }
