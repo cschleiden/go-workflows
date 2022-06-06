686cb894a71705b6fe5b5a58eee542147e6222ba
Christopher Schleiden <cschleiden@live.de>
2022-06-06 18:56:12 +0000

    Simple backend benchmark

--- backend/redis/redis_test.go
+++ backend/redis/redis_test.go
@@ -7,8 +7,13 @@ import (
 
 	"github.com/cschleiden/go-workflows/backend"
 	"github.com/cschleiden/go-workflows/backend/test"
+	"github.com/cschleiden/go-workflows/log"
 	"github.com/go-redis/redis/v8"
 )
+
+func Benchmark_RedisBackend(b *testing.B) {
+	test.SimpleWorkflowBenchmark(b, createBackend, nil)
+}
 
 func Test_RedisBackend(t *testing.T) {
 	if testing.Short() {
@@ -43,10 +48,37 @@ func createBackend() backend.Backend {
 		panic(err)
 	}
 
-	b, err := NewRedisBackend(address, user, password, 0, WithBlockTimeout(time.Millisecond*2))
+	b, err := NewRedisBackend(address, user, password, 0, WithBlockTimeout(time.Millisecond*2), WithBackendOptions(backend.WithLogger(&nullLogger{})))
 	if err != nil {
 		panic(err)
 	}
 
 	return b
 }
+
+type nullLogger struct {
+	defaultFields []interface{}
+}
+
+// Debug implements log.Logger
+func (*nullLogger) Debug(msg string, fields ...interface{}) {
+}
+
+// Error implements log.Logger
+func (*nullLogger) Error(msg string, fields ...interface{}) {
+}
+
+// Panic implements log.Logger
+func (*nullLogger) Panic(msg string, fields ...interface{}) {
+}
+
+// Warn implements log.Logger
+func (*nullLogger) Warn(msg string, fields ...interface{}) {
+}
+
+// With implements log.Logger
+func (nl *nullLogger) With(fields ...interface{}) log.Logger {
+	return nl
+}
+
+var _ log.Logger = (*nullLogger)(nil)
