95e8f0486e80ec44f9b92c410d280109cd9bf7aa
Christopher Schleiden <cschleiden@github.com>
2023-02-20 03:17:37 +0000

    Use more data for bench workflows

--- backend/test/bench.go
+++ backend/test/bench.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"context"
+	"log"
+	"math/rand"
 	"sync"
 	"testing"
 	"time"
@@ -13,15 +15,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const PayloadSizeBytes = 1 * 1024 * 1024 // 1 MB
+
+var alphabet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func randSeq(n int) string {
+	rand.Seed(42)
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(b)
+}
+
+var data = randSeq(PayloadSizeBytes)
+
 func SimpleWorkflowBenchmark(b *testing.B, setup func() TestBackend, teardown func(b TestBackend)) {
 	// Suppress default metric
 	b.ReportMetric(0, "ns/op")
 	b.StopTimer()
 
 	backend := setup()
-	if teardown != nil {
-		defer teardown(backend)
-	}
 
 	w := worker.New(backend, &worker.Options{
 		WorkflowPollers: 1,
@@ -40,6 +55,8 @@ func SimpleWorkflowBenchmark(b *testing.B, setup func() TestBackend, teardown fu
 
 	toStart := 100
 
+	log.Println("Starting", toStart, "workflows...")
+
 	wg := &sync.WaitGroup{}
 	for i := 0; i < toStart; i++ {
 		wg.Add(1)
@@ -52,39 +69,76 @@ func SimpleWorkflowBenchmark(b *testing.B, setup func() TestBackend, teardown fu
 
 	cancel()
 	require.NoError(b, w.WaitForCompletion())
+
+	if teardown != nil {
+		teardown(backend)
+	}
 }
 
 func startAndCompleteWorkflow(ctx context.Context, b *testing.B, c client.Client, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	wf, err := c.CreateWorkflowInstance(ctx, client.WorkflowInstanceOptions{
 		InstanceID: uuid.NewString(),
-	}, simpleWorkflow, 2)
+	}, simpleWorkflow, &simpleWorkflowInput{
+		In1:  1,
+		Data: data,
+	})
 	require.NoError(b, err)
 
-	r, err := client.GetWorkflowResult[int](ctx, c, wf, time.Second*30)
+	r, err := client.GetWorkflowResult[*simpleWorkflowResult](ctx, c, wf, time.Second*30)
 	require.NoError(b, err)
-	require.Equal(b, 5, r)
-
-	wg.Done()
+	require.Equal(b, 4, r.Result)
 }
 
-func simpleWorkflow(ctx workflow.Context, in1 int) (int, error) {
+type simpleWorkflowInput struct {
+	In1  int
+	Data string
+}
+
+type simpleWorkflowResult struct {
+	Result int
+	Data   string
+}
+
+func simpleWorkflow(ctx workflow.Context, input *simpleWorkflowInput) (*simpleWorkflowResult, error) {
 	r1, err := workflow.ExecuteActivity[int](ctx, workflow.DefaultActivityOptions, simpleActivity1).Get(ctx)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
-	r2, err := workflow.ExecuteActivity[int](ctx, workflow.DefaultActivityOptions, simpleActivity2).Get(ctx)
-	if err != nil {
-		return 0, err
+	var r2r int
+	for i := 0; i < 2; i++ {
+		r2, err := workflow.ExecuteActivity[*simpleActivity2result](ctx, workflow.DefaultActivityOptions, simpleActivity2).Get(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		r2r = r2.Result
 	}
 
-	return in1 + r1 + r2, nil
+	data, _ := workflow.SideEffect(ctx, func(ctx workflow.Context) string {
+		return data
+	}).Get(ctx)
+
+	return &simpleWorkflowResult{
+		Result: input.In1 + r1 + r2r,
+		Data:   data,
+	}, nil
 }
 
 func simpleActivity1(ctx context.Context) (int, error) {
 	return 1, nil
 }
 
-func simpleActivity2(ctx context.Context) (int, error) {
-	return 2, nil
+type simpleActivity2result struct {
+	Result int
+	Data   string
+}
+
+func simpleActivity2(ctx context.Context) (*simpleActivity2result, error) {
+	return &simpleActivity2result{
+		Result: 2,
+		Data:   data,
+	}, nil
 }
