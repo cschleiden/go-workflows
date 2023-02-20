ced2ac7406bd05f80b09c3bd96f7d6d8e0ce242d
Christopher Schleiden <cschleiden@github.com>
2023-02-20 04:56:46 +0000

    Add custom converter sample

---
+++ samples/converter/converter.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/cschleiden/go-workflows/backend"
+	"github.com/cschleiden/go-workflows/client"
+	"github.com/cschleiden/go-workflows/internal/converter"
+	"github.com/cschleiden/go-workflows/internal/payload"
+	"github.com/cschleiden/go-workflows/samples"
+	"github.com/cschleiden/go-workflows/worker"
+
+	"github.com/google/uuid"
+)
+
+type CustomConverter struct {
+}
+
+var _ converter.Converter = (*CustomConverter)(nil)
+
+func (*CustomConverter) From(data payload.Payload, v interface{}) error {
+	return gob.NewDecoder(bytes.NewReader(data)).Decode(v)
+}
+
+func (*CustomConverter) To(v interface{}) (payload.Payload, error) {
+	var buf bytes.Buffer
+
+	if err := gob.NewEncoder(&buf).Encode(v); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	b := samples.GetBackend("converter", backend.WithConverter(&CustomConverter{}))
+
+	// Run worker
+	w := RunWorker(ctx, b)
+
+	// Start workflow via client
+	c := client.New(b)
+
+	runWorkflow(ctx, c)
+
+	cancel()
+
+	if err := w.WaitForCompletion(); err != nil {
+		panic("could not stop worker" + err.Error())
+	}
+
+	// wait for sigint signal
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+	<-sigint
+}
+
+func runWorkflow(ctx context.Context, c client.Client) {
+	wf, err := c.CreateWorkflowInstance(ctx, client.WorkflowInstanceOptions{
+		InstanceID: uuid.NewString(),
+	}, Workflow1, "Hello world"+uuid.NewString(), 42, Inputs{
+		Msg:   "",
+		Times: 0,
+	})
+	if err != nil {
+		log.Fatal(err)
+		panic("could not start workflow")
+	}
+
+	result, err := client.GetWorkflowResult[int](ctx, c, wf, time.Second*10)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Workflow finished. Result:", result)
+}
+
+func RunWorker(ctx context.Context, mb backend.Backend) worker.Worker {
+	w := worker.New(mb, nil)
+
+	w.RegisterWorkflow(Workflow1)
+
+	w.RegisterActivity(Activity1)
+	w.RegisterActivity(Activity2)
+
+	if err := w.Start(ctx); err != nil {
+		panic("could not start worker")
+	}
+
+	return w
+}
