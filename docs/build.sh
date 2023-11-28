ea2b95f1826757d57bb23f683996da02cedf9929
Christopher Schleiden <cschleiden@live.de>
2023-11-27 21:41:06 -0800

    Image script

--- docs/build.sh
+++ docs/build.sh
@@ -6,4 +6,4 @@ docker run --rm --name slate -p 4567:4567 \
   -v "$(pwd)/source/includes":/srv/slate/source/includes \
   -v "$(pwd)/build":/srv/slate/build slatedocs/slate build
 
-cp -r ./images ./build/images
+cp -r ./images ./build/
