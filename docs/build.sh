c1270e3abcf5229f35b36f6ce8564407318bb226
Christopher Schleiden <cschleiden@live.de>
2023-11-27 21:44:05 -0800

    Create dir

--- docs/build.sh
+++ docs/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+mkdir -p build
+cp -r ./images ./build/
+
 docker run --rm --name slate -p 4567:4567 \
   -v "$(pwd)/source/logo.png":/srv/slate/source/images/logo.png \
   -v "$(pwd)/source/index.html.md":/srv/slate/source/index.html.md \
   -v "$(pwd)/source/includes":/srv/slate/source/includes \
   -v "$(pwd)/build":/srv/slate/build slatedocs/slate build
-
-cp -r ./images ./build/
