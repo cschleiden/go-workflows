470b50b1c189a0660f064d3c37e286a68374bcc7
Christopher Schleiden <cschleiden@live.de>
2024-02-24 21:08:43 -0800

    Support serving images

--- docs/serve.sh
+++ docs/serve.sh
@@ -4,4 +4,5 @@ docker run --rm --name slate -p 4567:4567 \
   -v "$(pwd)/source/logo.png":/srv/slate/source/images/logo.png \
   -v "$(pwd)/source/index.html.md":/srv/slate/source/index.html.md \
   -v "$(pwd)/source/includes":/srv/slate/source/includes \
-  -v "$(pwd)/build":/srv/slate/build slatedocs/slate serve
+  -v "$(pwd)/build":/srv/slate/build \
+  -v "$(pwd)/images":/srv/slate/source/images slatedocs/slate serve
