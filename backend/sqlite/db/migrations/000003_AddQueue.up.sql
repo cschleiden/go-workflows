80a14703315b4d939989663398cb49ada3ce53ad
Christopher Schleiden <cschleiden@live.de>
2024-06-03 21:08:19 -0700

    Update sqlite indexes

--- backend/sqlite/db/migrations/000003_AddQueue.up.sql
+++ backend/sqlite/db/migrations/000003_AddQueue.up.sql
@@ -11,3 +11,7 @@ ALTER TABLE `activities` ADD COLUMN `queue` NVARCHAR(128) DEFAULT '';
 -- Update index
 DROP INDEX IF EXISTS `idx_activities_instance_id_execution_id_worker`;
 CREATE INDEX `idx_activities_instance_id_execution_id_worker_queue` ON `activities` (`instance_id`, `execution_id`, `worker`, `queue`);
+
+DROP INDEX IF EXISTS `idx_activities_locked_until`;
+
+CREATE INDEX `idx_activities_locked_until_queue` ON `activities` (`locked_until`, `queue`);
