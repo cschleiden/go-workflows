f710b8f2860172453e01b7588b2f6585a2e84081
N/A <16502919+erma07@users.noreply.github.com>
2024-10-01 09:30:15 +0300

    Update 000001_initial.up.sql change create table if exists to create table

--- backend/postgres/db/migrations/000001_initial.up.sql
+++ backend/postgres/db/migrations/000001_initial.up.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS instances (
+DROP TABLE IF EXISTS instances;
+
+CREATE TABLE instances (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   instance_id UUID NOT NULL,
   execution_id UUID NOT NULL,
@@ -18,8 +20,8 @@ CREATE UNIQUE INDEX idx_instances_instance_id_execution_id on instances (instanc
 CREATE INDEX idx_instances_locked_until_completed_at on instances (completed_at, locked_until, sticky_until, worker);
 CREATE INDEX idx_instances_parent_instance_id_parent_execution_id ON instances (parent_instance_id, parent_execution_id);
 
-
-CREATE TABLE IF NOT EXISTS pending_events (
+DROP TABLE IF EXISTS pending_events;
+CREATE TABLE pending_events (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   event_id UUID NOT NULL,
   sequence_id BIGSERIAL NOT NULL, -- Not used, but keep for now for query compat
@@ -35,7 +37,7 @@ CREATE TABLE IF NOT EXISTS pending_events (
 CREATE INDEX idx_pending_events_inid_exid ON pending_events (instance_id, execution_id);
 CREATE INDEX idx_pending_events_inid_exid_visible_at_schedule_event_id ON pending_events (instance_id, execution_id, visible_at, schedule_event_id);
 
-
+DROP TABLE IF EXISTS history;
 CREATE TABLE IF NOT EXISTS history (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   event_id UUID NOT NULL,
@@ -52,6 +54,7 @@ CREATE TABLE IF NOT EXISTS history (
 CREATE INDEX idx_history_instance_id_execution_id ON history (instance_id, execution_id);
 CREATE INDEX idx_history_instance_id_execution_id_sequence_id ON history (instance_id, execution_id, sequence_id);
 
+DROP TABLE IF EXISTS activities;
 CREATE TABLE IF NOT EXISTS activities (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   activity_id UUID NOT NULL,
