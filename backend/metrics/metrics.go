61f3b3bd2a99ce61cbd1845365ced070c52d8540
cschleiden <cschleiden@live.de>
2024-11-10 15:21:06 -0800

    Add some docs

--- backend/metrics/metrics.go
+++ backend/metrics/metrics.go
@@ -5,13 +5,18 @@ import "time"
 type Tags map[string]string
 
 type Client interface {
+	//Counter records a value at a point in time.
 	Counter(name string, tags Tags, value int64)
 
+	// Distribution records a value at a point in time.
 	Distribution(name string, tags Tags, value float64)
 
+	// Gauge records a value at a point in time.
 	Gauge(name string, tags Tags, value int64)
 
+	// Timing records the duration of an event.
 	Timing(name string, tags Tags, duration time.Duration)
 
+	// WithTags returns a new client with the given tags applied to all metrics.
 	WithTags(tags Tags) Client
 }
